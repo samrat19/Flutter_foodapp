61ad1ab67dde651e62cd24110eabae97f3fd1c97
ATRI DAS <babinsamrat123@gmail.com>
2019-04-04 05:21:18 +0530

    commit +2

--- lib/data/rest_data.dart
+++ lib/data/rest_data.dart
@@ -5,4 +5,9 @@ class Rest_Data{
   Network_util network_util = Network_util();
 
   static final BASE_URL = "";
+  static final LOGIN_URL = BASE_URL+"/";
+
+  Future<User> login(String username, String password){
+    return null;
+  }
 }
