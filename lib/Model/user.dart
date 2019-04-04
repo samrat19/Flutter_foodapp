c25a5cee6c19da7511016fc5ace6b5745bc322dd
ATRI DAS <babinsamrat123@gmail.com>
2019-04-04 06:19:42 +0530

    commit +4

--- lib/Model/user.dart
+++ lib/Model/user.dart
@@ -1,5 +1,12 @@
 class User{
 
-  String username;
-  String password;
+  String _username;
+  String _password;
+
+  User(this._username,this._password);
+
+  User.map(dynamic obj){
+    this._username = obj['username'];
+    this._password = obj['password'];
+  }
 }
